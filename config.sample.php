89534a84810527c932882708c4e44a8b76c353de
Allison <allison@netbeez.net>
2019-03-08 14:28:52 -0500

    Adjustments to sample config.

--- config.sample.php
+++ config.sample.php
@@ -7,14 +7,14 @@
 //Optional error display for debugging (set to false for production)
 ini_set( "display_errors", false);
 
-//Name of the dashboard instance location
-define("DASHBOARD_LOCATION_NAME", "Your Location");
+//Name of the dashboard instance
+define("DASHBOARD_NAME", "NOC Grid");
 
 //The URL path for the dashboard instance
 define("URL_PATH", $_SERVER['REQUEST_URI']);
 
 //The host address of the NetBeez API (this is usually your NB dashboard's hostname)
-define("API_HOST", "YOUR_NETBEEZ_HOSTNAME");
+define("API_HOST", "https://<YOUR_NETBEEZ_SERVER_HOSTNAME>");
 
 //The NetBeez API version
 define("API_VERSION", "v1");
@@ -23,7 +23,7 @@ define("API_VERSION", "v1");
 define("API_AUTH_KEY", "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX");
 
 //Boolean setting for cURL option to verify the SSL host
-define("SSL_VERIFY_HOST", false);
+define("SSL_VERIFY_HOST", true);
 
 //Boolean setting for cURL option to verify the SSL peer.
 //Default is false due to issue with certificate configuration on internal NetBeez instances
